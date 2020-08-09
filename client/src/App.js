cf24298f686b1ca821b0f218534097a6c18fa6b8
Sam Dratch <dratch.sam1@gmail.com>
2020-08-09 16:34:57 -0400

    added pages and routers for the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 
 function App() {
+  useEffect(() => {
+    
+    axios.get("/api/config").then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route path="/saved" component={Saved} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </Router>
     </div>
   );
 }
